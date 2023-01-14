0d785bb724753db9cbb151a8a855efc64f643e97
oliverJurgen <oliver.e@scopicsoftware.com>
2023-01-14 14:57:27 -0500

    add config values

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "us-east-2_Ys8QY9Y3O", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "2gsu0326qqfcnoa9d1qnfo1pm0", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-2", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
